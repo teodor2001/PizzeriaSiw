da5d154e956e6953a22062e38a5911e59fc44e76
Teo <teodorpascu2001@gmail.com>
2025-05-05 12:55:53 +0200

    Database con prima classe di test

---
+++ src/main/java/it/uniroma3/siw/model/Pizzeria.java
@@ -0,0 +1,68 @@
+package it.uniroma3.siw.model;
+
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Pizzeria {
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	private Long id;
+    private String nome;
+    private String indirizzo;
+    private String citta;
+    private String email;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getIndirizzo() {
+		return indirizzo;
+	}
+	public void setIndirizzo(String indirizzo) {
+		this.indirizzo = indirizzo;
+	}
+	public String getCitta() {
+		return citta;
+	}
+	public void setCitta(String citta) {
+		this.citta = citta;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(citta, email, id, indirizzo, nome);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pizzeria other = (Pizzeria) obj;
+		return Objects.equals(citta, other.citta) && Objects.equals(email, other.email) && Objects.equals(id, other.id)
+				&& Objects.equals(indirizzo, other.indirizzo) && Objects.equals(nome, other.nome);
+	}
+	
+	
+
+}
