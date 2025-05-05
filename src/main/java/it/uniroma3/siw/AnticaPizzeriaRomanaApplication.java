dfad2f47f9f60600f66b1d4427320c52eba6155c
Teo <teodorpascu2001@gmail.com>
2025-05-05 12:00:35 +0200

    Progetto Inizializzato con Spring Initializr
    Primo push con il progetto Spring base e le varie dipendenze

---
+++ src/main/java/it/uniroma3/siw/AnticaPizzeriaRomanaApplication.java
@@ -0,0 +1,13 @@
+package it.uniroma3.siw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnticaPizzeriaRomanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnticaPizzeriaRomanaApplication.class, args);
+	}
+
+}
