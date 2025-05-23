14ac0eaadc1f1d9d1d223e121e3df0dc11b66d5f
Teo <teodorpascu2001@gmail.com>
2025-05-23 15:31:05 +0200

    DB spostato su AWS e messe le relative variabili d'ambiente

--- src/main/java/it/uniroma3/siw/AnticaPizzeriaRomanaApplication.java
+++ src/main/java/it/uniroma3/siw/AnticaPizzeriaRomanaApplication.java
@@ -2,10 +2,7 @@ package it.uniroma3.siw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
-import it.uniroma3.siw.config.XmlPropertySourceFactory;
 @SpringBootApplication
-@PropertySource(value = "classpath:secrets.xml", factory = XmlPropertySourceFactory.class, ignoreResourceNotFound = true)
 public class AnticaPizzeriaRomanaApplication {
 
 	public static void main(String[] args) {
